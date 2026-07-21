cd047d973aa83bda3eb851133bdbe98b34133cbd
laiba batool <laibashah399@gmail.com>
2026-07-21 12:18:02 +0500

    updated

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -5,20 +5,20 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="bg-gray-200 flex items-center justify-center min-h-screen">
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
     
-    <div class="bg-white p-12"> 
-        <h2> Admin Login </h2>
-        <form action="" method="POST">
+    <div class="bg-white p-8 rounded-xl shadow-lg w-full max-w-sm"> 
+        <h2 class="text-2xl text-center text-gray-800 mb-6"> Admin Login </h2>
+        <form action="" method="post" class="space-y-4">
             <div>
-                <label for="">Admin name:</label>
-                <input type="text" placeholder="Enter Admin name">
+                <label for="" class="text-gray-600 mb-1">Admin name:</label>
+                <input type="text" placeholder="Enter Admin name" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
             </div>
          <div>
-            <label for="">Admin Password:</label>
-            <input type="password" placeholder="Enter Admin Password">
+            <label for="" class="text-gray-600 mb-1">Admin Password:</label>
+            <input type="password" placeholder="Enter Admin Password"  class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
          </div>
-         <button type="submit">Login</button>
+         <button type="submit" class="bg-blue-500 w-full text-white py-2 px-4 rounded-xl hover:bg-blue-600">Login</button>
         </form>
     </div>
 </body>
