eef64ef4d6cb0928c8ed411d88cbfd086a240a27
laiba batool <laibashah399@gmail.com>
2026-09-02 10:40:37 +0500

    Fix quiz MCQ view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -107,9 +107,11 @@ function addQuiz(){
         
         }else{
            $quiz= Session::get('quizDetails');
-           $totalMCQs =$quiz && Mcq::where('quiz_id',$quiz->id)->count();
+        //    $totalMCQs =$quiz && Mcq::where('quiz_id',$quiz->id)->count();
         }
-
+      if($quiz){
+    $totalMCQs = Mcq::where('quiz_id', $quiz->id)->count();
+      }
         return view('add-quiz',["name"=>$admin->name, "categories"=>$categories, "totalMCQs"=>$totalMCQs]);
     }else{
         return redirect('admin-login');
@@ -156,13 +158,25 @@ function addQuiz(){
         return redirect("/admin-categories");
 
     }
-    function showQuiz($id){
+    function showQuiz($id,$quizName){
         
          $admin = Session::get('admin');  
          $mcqs=Mcq::where('quiz_id',$id)->get();  
          if($admin){
 
-            return view('show-quiz',["name"=>$admin->name,"mcqs"=>$mcqs]);
+            return view('show-quiz',["name"=>$admin->name,"mcqs"=>$mcqs,"quizName"=>$quizName]);
+         }else{
+             return redirect('admin-login');
+         }
+
+    }
+
+    function quizList($id,$category){
+         $admin = Session::get('admin');  
+         $quizData=Quiz::where('category_id',$id)->get();  
+         if($admin){
+
+            return view('quiz-list',["name"=>$admin->name,"quizData"=>$quizData,"category"=>$category]);
          }else{
              return redirect('admin-login');
          }
